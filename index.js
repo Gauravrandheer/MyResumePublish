8857b4883142d1a3d3b435781df3535e132a0143
gauravRandheer <gaurav.shrivastava.15csc@bml.edu.in>
2025-06-08 13:22:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const fs = require("fs")
+const path = require("path")
+
+const resumePath = path.join(__dirname,"resume.txt")
+
+fs.readFile(resumePath,'utf-8',(err,data)=>{
+    if(err){
+        console.log(err)
+        return;
+    }
+    console.log(data)
+})
